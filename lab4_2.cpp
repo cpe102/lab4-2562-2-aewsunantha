db50598c4406f4453171b2f58250a69e8777fbc9
unknown <sunantha1881@gmail.com>
2020-01-03 17:05:57 +0700

    edit4

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
 //Write function findDistance() here
+float findS(float u,float a,float t){
+  float s = (u*t)+(0.5*a*(t*t));
+  return s;
+}
 
 int main(){
-
+float a,u,t;
   //Calling findDistance() using test cases
-
+  
+  cout<<"s = "<<findS(0,0.5,1);
   return 0;
 }
--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int findDivisor(int x){
+    int i=2;
+    while(i<x){
+       if(x%i==0){
+        return i;   
+       }else{
+          i++; 
+       }
+       
+       
+    }
+}
 
+int main(){
+    cout<<findDivisor(221);
+    return 0;
+}
